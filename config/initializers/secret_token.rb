2e391d5aefe8ba3e199def88d7647ef1c6ef54d5
ChelseyFrank <chelsey@zaarly.com>
2014-03-01 14:15:50 -0800

    My first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beautytube::Application.config.secret_token = 'c29692ce4bd6b5bc01598b555ba3e03ba359cc505dae4ab303ea36162745f0e0b9ced5a4959dd9789fa7bafb80f91d25ee6af315823c0fd98010be3e3b56843b'
