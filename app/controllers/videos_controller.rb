9b1308a8ed5cc629958322f98034073c65b75bcc
ChelseyFrank <chelsey@zaarly.com>
2014-03-01 16:06:33 -0800

    beefed up show action

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,9 @@
 class VideosController < ApplicationController
   def show
+		puts Video.get('/feeds/api/videos', :query => {:q => 'hair'})
+
+		puts Video.get_all.inpsect # will be displayed in your logs (most likely)
+		raise Video.get_all.inspect # will raise the response to the view
   end
 
   def index
