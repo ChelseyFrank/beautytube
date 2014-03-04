7b78a9dc23d6be293db92c211f06ba3f01ca6a43
ChelseyFrank <chelsey@zaarly.com>
2014-03-03 19:14:16 -0800

    Controller work & parsing response from Youtube api

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,12 @@
 class VideosController < ApplicationController
   def show
     @query = params[:query]
-    @results = Video.get('/feeds/api/videos', :query => {:q => @query})
+    @results = Video.get('/feeds/api/videos', :query => {:q => @query, :alt => 'json'})
+    @first_url = @results["feed"]["entry"][0]["id"]["$t"]
+    # @first_url = @results["feed"]["entry"][0]["media$group"]["media$player"][0]["url"]
+
+    @id = @first_url.split("videos/")[1]
+  #   @url = "https://www.youtube.com/v/VzEHmcABGUU?version=3&f=videos&app=youtube_gdata"
   end
 
   def index
