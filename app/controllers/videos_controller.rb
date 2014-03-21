227cb4f1674a779fb1e193d3a3bb9be4f009eb9f
ChelseyFrank <chelsey@zaarly.com>
2014-03-21 15:30:07 -0700

    more work on the show with a next link

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,13 @@
 class VideosController < ApplicationController
   def show
     @query = params[:query]
-    @randnumber = rand(0...20)
-    @results = Video.get('/feeds/api/videos', :query => {:q => @query, :alt => 'json', :max_results => 50})
+    @randnumber = rand(0...10)
+    @page = rand(0...100)
+    @results = Video.get('/feeds/api/videos?&max-results=10&start-index='+ "#{@page}", :query => {:q => @query, :alt => 'json'})
     @first_url = @results["feed"]["entry"][@randnumber]["id"]["$t"]
     @video_title = @results["feed"]["entry"][@randnumber]["title"]["$t"]
-    # @video_description = @results["feed"]["entry"][@randnumber]["content"]["$t"]
+    # @first_url_long = @results["feed"]["link"][3]["href"]
+    # @second_url_long = @results["feed"]["link"][4]["href"]
 
     @id = @first_url.split("videos/")[1]
     @url = "https://www.youtube.com/embed/" + @id
@@ -15,7 +17,7 @@ class VideosController < ApplicationController
     @braid = "how to hair braids"
     @color = "hair coloring styles"
     @eye = "eye makeup tutorial"
-    @lip = "lip makeup techniques"
+    @lip = "lip%20makeup%7Clipstick%20tutorial"
     @nail = "nail painting tricks"
   end
 end
